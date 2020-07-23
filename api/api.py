e97077859c3ef3abfadb21baf0abf09a8fabd676
B <w6u03@students.keele.ac.uk>
2020-07-23 03:14:02 +0100

    change api

--- api/api.py
+++ api/api.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 
-@app.route('/time')
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
 
