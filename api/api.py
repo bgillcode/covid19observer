3427004fc8f1b87eef4ea9a5489db4b15972cf04
lpe <n@lpes-MacBook-Air.local>
2020-07-23 02:33:16 +0100

    Got

--- api/api.py
+++ api/api.py
@@ -1,8 +1,18 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+#if __name__ == "__main__":
+#    # Only for debugging while developing
+#    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
