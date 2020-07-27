10519d35a8e2eda6089a7243e393a825c675415f
bgillcode <b.gill.messages@gmail.com>
2020-07-27 12:19:51 +0100

    Refactor the routes to use far less lines and use var for collection
    Added comments throughout too

--- api/api.py
+++ api/api.py
@@ -16,8 +16,11 @@ import re
 import key
 
 
-app = Flask(__name__, static_folder='../build', static_url_path='/')
+# Note: Parameters that are passed to the route are kept as lowercase to
+# differentiate them from other variables created in the function
 
+
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 app.config["MONGO_URI"] = key.CONNECTION_STRING
 mongo = PyMongo(app)
 
@@ -29,12 +32,14 @@ def index():
 
 @app.route('/apic/getoverview')
 def index3():
+    # Note: Parameters that are passed to the route are kept as lowercase to
+    # differentiate them from other variables created in the function
     dategiven = request.args.get('dategiven', default = '*', type = str)
-    placegiven = request.args.get('placegiven', default = '*', type = str)
+    areanamegiven = request.args.get('areanamegiven', default = '*', type = str)
     areatype = request.args.get('areatype', default = '*', type = str)
 
     print(dategiven)
-    print(placegiven)
+    print(areanamegiven)
     print(areatype)
 
     # Check if date given in the parameter route is valid first
@@ -50,155 +55,75 @@ def index3():
     dbss = mongo.cx['placedata']
 
     # This variable controls how many maximum results are returned from queries
-    limitAmount = 300
-    limitAmountltla = 400
-    limitAmountutla = 300
+    limitAmount = 100
 
     areaNameOfOverview = "United Kingdom"
 
-    docs = []
-    # If the area type was given
+    collectionNameUsed = ''
+
+    # Switch to see if area type has been selected from a predefined list
+    areatypeinlist = 0
+
     if areatype == 'ltla':
-        # If no name was given for the area
-        if placegiven == '*':
+        # The collection name is set here to be searched through depending on the parameter that was passed, in this case for areatype
+        collectionNameUsed = 'ltla_cases'
+        # Used to limit the amount of search results returned for the different cases
+        limitAmount = 400
+        # Switch to see if area type has been selected from a predefined list
+        areatypeinlist = 1
+    if areatype == 'utla':
+        collectionNameUsed = 'utla_cases'
+        limitAmount = 300
+        areatypeinlist = 1
+    if areatype == 'region':
+        collectionNameUsed = 'regions_cases'
+        limitAmount = 1000
+        areatypeinlist = 1
+    if areatype == 'nation':
+        collectionNameUsed = 'nations_overview'
+        limitAmount = 1000
+        areatypeinlist = 1
+    if areatype == 'nhsregion':
+        collectionNameUsed = 'nhsregions_overview'
+        limitAmount = 1000
+        areatypeinlist = 1
+    if areatype == 'overview':
+        collectionNameUsed = 'unitedkingdom_overview'
+        limitAmount = 1000
+        areatypeinlist = 1
+
+    # Used to store the documents that are returned
+    docs = []
+
+    # If the area type was in the predefined list
+    if areatypeinlist == 1:
+        # If no place name was given
+        if areanamegiven == '*':
+            # If the date was not given
             if dategiven == '*':
                 # Search by just area
-                # Get the results for just this parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.ltla_cases.find({ "areaType" : areatype }, { '_id' : False }).sort("date", -1).limit(limitAmountltla)
+                # Get the results for just this parameter given (for this areatype)
+                queryCreated = dbss[collectionNameUsed].find({ "areaType" : areatype }, { '_id' : False }).sort("date", -1).limit(limitAmount)
             else:
                 # Search by just date
-                # Get 100 results for the parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.ltla_cases.find({ "date" : start }, { '_id' : False }).limit(limitAmountltla)
+                # Get the results for the parameter given (for this areatype)
+                queryCreated = dbss[collectionNameUsed].find({ "date" : start }, { '_id' : False }).limit(limitAmount)
 
         # If a place name was given
         else:
-            # If no date was given, search for just the areaname (for this areatype)
-            if dategiven == '*':
-                queryCreated = dbss.ltla_cases.find({ "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmountltla)
-            # If no date was given, search for just the areaname (for this areatype)
-            else:
-                # If no date was given, search for the date and areaname (for this areatype)
-                queryCreated = dbss.ltla_cases.find({ "date" : start, "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmountltla)
+            # If the area type is not 'overview', i.e. if it's not for the United Kingdom, do not want to pass an area name because this is already set
+            if areatype != 'overview':
+                # If no date was given, search for just the areaname (for this areatype)
+                if dategiven == '*':
+                    queryCreated = dbss[collectionNameUsed].find({ "areaName" : re.compile(areanamegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmount)
+                # If no date was given, search for just the areaname (for this areatype)
+                else:
+                    # If no date was given, search for the date and areaname (for this areatype)
+                    queryCreated = dbss[collectionNameUsed].find({ "date" : start, "areaName" : re.compile(areanamegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmount)
 
         # Put the documents returned into the array (append)
         for doc in queryCreated:
             docs.append(doc)
-    # If the area given was utla
-    elif areatype == 'utla':
-        # If no name was given for the area
-        if placegiven == '*':
-            if dategiven == '*':
-                # Search by just area
-                # Get the results for just this parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.utla_cases.find({ "areaType" : areatype }, { '_id' : False }).sort("date", -1).limit(limitAmountutla)
-            else:
-                # Search by just date
-                # Get 100 results for the parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.utla_cases.find({ "date" : start }, { '_id' : False }).limit(limitAmountutla)
-
-        # If a place name was given
-        else:
-            # If no date was given, search for just the areaname (for this areatype)
-            if dategiven == '*':
-                queryCreated = dbss.utla_cases.find({ "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmountutla)
-            # If no date was given, search for just the areaname (for this areatype)
-            else:
-                # If no date was given, search for the date and areaname (for this areatype)
-                queryCreated = dbss.utla_cases.find({ "date" : start, "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmountutla)
-
-        # Put the documents returned into the array (append)
-        for doc in queryCreated:
-            docs.append(doc)
-    elif areatype == 'region':
-        # If no name was given for the area
-        if placegiven == '*':
-            if dategiven == '*':
-                # Search by just area
-                # Get the results for just this parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.regions_cases.find({ "areaType" : areatype }, { '_id' : False }).sort("date", -1).limit(limitAmount)
-            else:
-                # Search by just date
-                # Get 100 results for the parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.regions_cases.find({ "date" : start }, { '_id' : False }).limit(limitAmount)
-
-        # If a place name was given
-        else:
-            # If no date was given, search for just the areaname (for this areatype)
-            if dategiven == '*':
-                queryCreated = dbss.regions_cases.find({ "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmount)
-            # If no date was given, search for just the areaname (for this areatype)
-            else:
-                # If no date was given, search for the date and areaname (for this areatype)
-                queryCreated = dbss.regions_cases.find({ "date" : start, "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmount)
-
-        # Put the documents returned into the array (append)
-        for doc in queryCreated:
-            docs.append(doc)
-    elif areatype == 'nation':
-        # If no name was given for the area
-        if placegiven == '*':
-            if dategiven == '*':
-                # Search by just area
-                # Get the results for just this parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.nations_overview.find({ "areaType" : areatype }, { '_id' : False }).sort("date", -1).limit(limitAmount)
-            else:
-                # Search by just date
-                # Get 100 results for the parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.nations_overview.find({ "date" : start }, { '_id' : False }).limit(limitAmount)
-
-        # If a place name was given
-        else:
-            # If no date was given, search for just the areaname (for this areatype)
-            if dategiven == '*':
-                queryCreated = dbss.nations_overview.find({ "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmount)
-            # If no date was given, search for just the areaname (for this areatype)
-            else:
-                # If no date was given, search for the date and areaname (for this areatype)
-                queryCreated = dbss.nations_overview.find({ "date" : start, "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmount)
-
-        # Put the documents returned into the array (append)
-        for doc in queryCreated:
-            docs.append(doc)
-    elif areatype == 'nhsregion':
-        # If no name was given for the area
-        if placegiven == '*':
-            if dategiven == '*':
-                # Search by just area
-                # Get the results for just this parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.nhsregions_overview.find({ "areaType" : areatype }, { '_id' : False }).sort("date", -1).limit(limitAmount)
-            else:
-                # Search by just date
-                # Get 100 results for the parameter given (this is because there would be too many to return for this one) (for this areatype)
-                queryCreated = dbss.nhsregions_overview.find({ "date" : start }, { '_id' : False }).limit(limitAmount)
-
-        # If a place name was given
-        else:
-            # If no date was given, search for just the areaname (for this areatype)
-            if dategiven == '*':
-                queryCreated = dbss.nhsregions_overview.find({ "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmount)
-            # If no date was given, search for just the areaname (for this areatype)
-            else:
-                # If no date was given, search for the date and areaname (for this areatype)
-                queryCreated = dbss.nhsregions_overview.find({ "date" : start, "areaName" : re.compile(placegiven, re.IGNORECASE) }, { '_id' : False }).limit(limitAmount)
-
-        # Put the documents returned into the array (append)
-        for doc in queryCreated:
-            docs.append(doc)
-    elif areatype == 'overview':
-        # If no name was given for the area
-        if dategiven == '*':
-            # Search by just area
-            # Get the results for just this parameter given (this is because there would be too many to return for this one) (for this areatype)
-            queryCreated = dbss.unitedkingdom_overview.find({"areaName": areaNameOfOverview }, { '_id' : False }).sort("date", -1).limit(limitAmount)
-        else:
-            # Search by just date
-            # Get 100 results for the parameter given (this is because there would be too many to return for this one) (for this areatype)
-            queryCreated = dbss.unitedkingdom_overview.find({ "date" : start, "areaName": areaNameOfOverview }, { '_id' : False }).limit(limitAmount)
-
-        # Put the documents returned into the array (append)
-        for doc in queryCreated:
-            docs.append(doc)
-
 
     # If results were returned (i.e. not an empty array), return the results as a json
     if len(docs) > 0:
@@ -207,7 +132,6 @@ def index3():
     else:
         # Return the result as a json file with the
         return 'No result'
-
 
 
 @app.route('/apic/getpolicy')
@@ -232,8 +156,6 @@ def index4():
 
     # This variable controls how many maximum results are returned from queries
     limitAmount = 300
-    limitAmountltla = 400
-    limitAmountutla = 300
 
     areaNameOfOverview = "United Kingdom"
 
