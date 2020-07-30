914c3f9ac64586ee0ef4d5242cb58673cec0c76f
bgillcode <b.gill.messages@gmail.com>
2020-07-30 02:23:06 +0100

    Add figures + fix mouseover events and fix layers add popup/remove popup

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import NativeSelect from '@material-ui/core/NativeSelect';
 
+import Choropleth from 'react-leaflet-choropleth'
+
 import axios from 'axios';
 
 import L from "leaflet";
@@ -30,20 +32,35 @@ import CountriesNew from "./countries"
 
 class App extends Component {
 
-  state = {
-    language: '',
-    areaName: 'United Kingdom',
-    areaType: 'overview',
-    search: '',
-    dataForSearch: ['United Kingdom', 'England', 'Northern Ireland', 'Scotland', 'Wales'],
-    dataGottenBackFromAPI: [],
-    chosenFromDropdownArray: 10,
+  constructor(props) {
+    super(props)
 
-    dataGottenBackPlaces: [],
 
-    latOfArea: "54.7023545",
-    lonOfArea: "-3.2765753",
-    valueOfZoom: 5,
+    this.state = {
+      language: '',
+      areaName: 'United Kingdom',
+      areaType: 'overview',
+      search: '',
+      dataForSearch: ['United Kingdom', 'England', 'Northern Ireland', 'Scotland', 'Wales'],
+      dataGottenBackFromAPI: [],
+      chosenFromDropdownArray: 10,
+
+      dataGottenBackPlaces: [],
+
+      latOfArea: "54.7023545",
+      lonOfArea: "-3.2765753",
+      valueOfZoom: 5,
+      dataForMapGotten: [],
+      dataForMapGottenNation: [],
+      loadedBorderInfo: 0,
+    }
+
+    this.geojson = L.geoJson();
+
+    this.getColor = this.getColor.bind(this);
+    this.style = this.style.bind(this);
+
+    this.gotNewNumber = this.gotNewNumber.bind(this);
   }
 
   handleLanguage = (langValue) => {
@@ -59,27 +76,158 @@ class App extends Component {
   }
 
   getColor(d) {
-    return d > 1000 ? '#800026' :
-    d > 500  ? '#BD0026' :
-    d > 200  ? '#E31A1C' :
-    d > 100  ? '#FC4E2A' :
-    d > 50   ? '#FD8D3C' :
-    d > 20   ? '#FEB24C' :
-    d > 10   ? '#FED976' :
-    '#FFEDA0';
-
+    console.log(d);
+    return d > 200000 ? '#800026' :
+    d > 18000  ? '#FD8D3C' :
+    d > 17000  ? '#FEB24C' :
+    d > 5000  ? '#99cc99' :
+    d > 50   ? '#FEECBB' :
+    d > 20   ? '#FEF7E3' :
+    d > 10   ? '#E3EAFE' :
+    '#99cc99';
   }
 
   style(feature) {
+    var gottenFeature = ''
+    var gottenFeatureNumber = 0
+    console.log(feature);
+    console.log(this.state.dataForMapGottenNation.length);
+    if (this.state.dataForMapGottenNation.length > 0) {
+      console.log(this.state.dataForMapGottenNation[0]);
+      console.log(this.state.dataForMapGottenNation[0].data);
+      for (let dataFromStateArray of this.state.dataForMapGottenNation[0].data) {
+        console.log(dataFromStateArray);
+        if (dataFromStateArray.areaName == feature.properties.ctry19nm) {
+          gottenFeature = dataFromStateArray.areaName
+          gottenFeatureNumber = dataFromStateArray.cumCasesByPublishDate
+          console.log(gottenFeature + ": " + gottenFeatureNumber);
+          break;
+        }
+      }
+    }
+
     return {
-      fillColor: this.getColor(250),
-      weight: 2,
-      opacity: 0.3,
-      color: 'white',
+      fillColor: this.getColor(gottenFeatureNumber),
+      weight: 3,
+      opacity: 0.5,
+      color: 'black',
       dashArray: '3',
       fillOpacity: 0.2
     };
   }
+
+
+  gotNewNumber(feature) {
+    var gottenFeature = ''
+    var gottenFeatureNumber = 0
+    console.log(feature);
+    console.log(this.state.dataForMapGottenNation.length);
+    if (this.state.dataForMapGottenNation.length > 0) {
+      console.log(this.state.dataForMapGottenNation[0]);
+      console.log(this.state.dataForMapGottenNation[0].data);
+      for (let dataFromStateArray of this.state.dataForMapGottenNation[0].data) {
+        console.log(dataFromStateArray);
+        if (dataFromStateArray.areaName == feature.properties.ctry19nm) {
+          gottenFeature = dataFromStateArray.areaName
+          gottenFeatureNumber = dataFromStateArray.cumCasesByPublishDate
+          console.log(gottenFeature + ": " + gottenFeatureNumber);
+          break;
+        }
+      }
+    }
+
+    return gottenFeatureNumber
+  }
+
+
+
+
+  highlightFeature(e) {
+    console.log(e);
+    var layer = e.target;
+
+    layer.setStyle({
+      weight: 5,
+      color: '#666',
+      dashArray: '',
+      fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+      layer.bringToFront();
+      // layer.bindPopup("a");
+    }
+
+
+    var gottenFeature = ''
+    var gottenFeatureNumber = 0
+    console.log(layer);
+    console.log(this.state.dataForMapGottenNation.length);
+    console.log(this);
+    if (this.state.dataForMapGottenNation.length > 0) {
+      console.log(this.state.dataForMapGottenNation[0]);
+      console.log(this.state.dataForMapGottenNation[0].data);
+      for (let dataFromStateArray of this.state.dataForMapGottenNation[0].data) {
+        console.log(dataFromStateArray);
+        if (dataFromStateArray.areaName == layer.feature.properties.ctry19nm) {
+          gottenFeature = dataFromStateArray.areaName
+          gottenFeatureNumber = dataFromStateArray.cumCasesByPublishDate
+          console.log(gottenFeature + ": " + gottenFeatureNumber);
+          break;
+        }
+      }
+    }
+
+
+
+    layer.bindPopup(gottenFeature + ": " + gottenFeatureNumber);
+    // e.openPopup();
+    layer.openPopup();
+  }
+
+  resetHighlight(e) {
+    console.log(this.geojson);
+    // component
+    console.log(e);
+
+    var feature = e.target.feature;
+
+
+    var gottenFeature = ''
+    var gottenFeatureNumber = 0
+    console.log(feature);
+    console.log(this.state.dataForMapGottenNation.length);
+    if (this.state.dataForMapGottenNation.length > 0) {
+      console.log(this.state.dataForMapGottenNation[0]);
+      console.log(this.state.dataForMapGottenNation[0].data);
+      for (let dataFromStateArray of this.state.dataForMapGottenNation[0].data) {
+        console.log(dataFromStateArray);
+        if (dataFromStateArray.areaName == feature.properties.ctry19nm) {
+          gottenFeature = dataFromStateArray.areaName
+          gottenFeatureNumber = dataFromStateArray.cumCasesByPublishDate
+          console.log(gottenFeature + ": " + gottenFeatureNumber);
+          break;
+        }
+      }
+    }
+
+    e.target.setStyle({
+      fillColor: this.getColor(gottenFeatureNumber),
+      weight: 3,
+      opacity: 0.5,
+      color: 'black',
+      dashArray: '3',
+      fillOpacity: 0.2
+    });
+
+    e.target.closePopup();
+  }
+
+  zoomToFeature(e) {
+    Maps.fitBounds(e.target.getBounds());
+  }
+
+
 
   createClusterCustomIcon(cluster) {
     var markers = cluster.getAllChildMarkers();
@@ -105,14 +253,14 @@ class App extends Component {
     });
   }
 
-//   getStyle(feature, layer) {
-//     console.log(feature);
-//   return {
-//     color: '#022450',
-//     weight: 2,
-//     opacity: 0.1
-//   }
-// }
+  //   getStyle(feature, layer) {
+  //     console.log(feature);
+  //   return {
+  //     color: '#022450',
+  //     weight: 2,
+  //     opacity: 0.1
+  //   }
+  // }
 
   updateSearch(event) {
     console.log(event.target.value);
@@ -122,6 +270,13 @@ class App extends Component {
       search: event.target.value.substr(0, 20)
     })
 
+  }
+
+  updateMapBorders() {
+    this.setState({
+      loadedBorderInfo: 1
+    });
+    console.log(this.state.dataForMapGottenNation);
   }
 
   displayTextBox() {
@@ -195,37 +350,65 @@ class App extends Component {
   };
 
   handleChange(event) {
-  const name = event.target.value;
-  console.log(name);
-  this.setState({
-    chosenFromDropdownArray: name,
-  });
-  console.log(this);
-};
+    const name = event.target.value;
+    console.log(name);
+    this.setState({
+      chosenFromDropdownArray: name,
+    });
+    console.log(this);
+  };
+
+  onEachFeature(feature, layer) {
+    // console.log(f);
+    console.log(feature);
+    layer.on({
+      mouseover: this.highlightFeature.bind(this),
+      mouseout: this.resetHighlight.bind(this),
+      click: this.zoomToFeature
+    });
+
+
+
+
+    // layer.bindPopup("Cumulative Daily Cases: " + gottenFeature + ": " + gottenFeatureNumber);
+  }
 
 
   // Get the data and then run the update for the chart to be displayed for each one
   componentDidMount() {
-      var urlGotten = 'http://localhost:5000/apic/getoverview?areatype=' + this.state.areaType + '&areanamegiven=' + this.state.areaName
+    var baseURL = 'http://localhost:5000'
+    var apiName = 'apic'
+    var getRoute = 'getoverview'
+    var getFieldForRoute1 = 'areatype'
+    var getFieldForRoute2 = 'areanamegiven'
+    var urlGotten = baseURL + '/' + apiName + '/' + getRoute + '?' + getFieldForRoute1 + '=' + this.state.areaType + '&' + getFieldForRoute2 + '=' + this.state.areaName
 
-      console.log(urlGotten);
+    console.log(urlGotten);
 
-      fetch(urlGotten).then(res => res.json()).then(data => {
-        var joined = this.state.dataGottenBackPlaces.concat(data)
-        console.log(data);
-        // var dataToAdd = data;
+    fetch(urlGotten).then(res => res.json()).then(data => {
+      var joined = this.state.dataGottenBackPlaces.concat(data)
+      console.log(data);
+
+    }).then( joined => {
+      this.setState({
+        dataForMapGotten: joined,
       })
+    });
 
 
     var urlGotten2 = 'http://localhost:5000/apic/getoverview?areatype=' + 'nation'
 
     fetch(urlGotten2).then(res => res.json()).then(data => {
-      var joined = this.state.dataGottenBackPlaces.concat(data)
-      console.log(data);
-      // var dataToAdd = data;
+      var joined = this.state.dataForMapGottenNation.concat(data)
+      console.log(data.data);
+      return joined;
+    }).then( joined => {
+      this.setState({
+        dataForMapGottenNation: joined,
+      })
+    }).then( () => {
+      this.updateMapBorders();
     })
-
-
   }
 
 
@@ -304,73 +487,78 @@ class App extends Component {
                   <Marker position={[54.3013545, -3.2764753]} />
 
                   <Marker position={[51.7023545, -3.8765753]}>
+                  <Popup
+                  minWidth={200}
+                  closeButton={false}
+                  onClose={popup => console.warn('popup-close', popup)}
+                  >
+                  <div>
+                  <b>Hello world!</b>
+                  <p>stuff</p>
+                  </div>
+                  </Popup>
+
+                  </Marker>
+
+                  </MarkerClusterGroup>
+                  */}
+
+                  <MarkerClusterGroup iconCreateFunction={this.createClusterCustomIcon}>
+
+                    <Marker position={[59.3023545, -3.2765753]} number={5}>
+                      <Popup
+                        minWidth={200}
+                        closeButton={false}
+                        onClose={popup => console.warn('popup-close', popup)}
+                        >
+                        <div>
+                          <b>Location: Hello world2!</b>
+                          <p>Count: {4}</p>
+                        </div>
+                      </Popup>
+                    </Marker>
+
+                    <Marker position={[51.7323545, -3.2765753]} number={5} />
+
+                    <Marker position={[55.7323545, -1.2765753]} number={8} />
+                    <Marker position={[51.7323545, -1.2755753]} number={1000.23} />
+
+                  </MarkerClusterGroup>
+
+                  <CircleLeaflet center={[51.7323545, -3.2765753]} radius={5000} attribution="hello" >
                     <Popup
                       minWidth={200}
                       closeButton={false}
                       onClose={popup => console.warn('popup-close', popup)}
                       >
                       <div>
-                        <b>Hello world!</b>
+                        <b>Hello world2!</b>
                         <p>stuff</p>
                       </div>
                     </Popup>
 
-                  </Marker>
+                  </CircleLeaflet>
 
-                </MarkerClusterGroup>
-                */}
+                  {/* // <GeoJSON data={this.getGeoJSONNew()} style={this.getStyle(1, 2)} /> */}
+                  <GeoJSON data={this.getGeoJSONNew()} style={this.style} onEachFeature={this.onEachFeature.bind(this)} ref="geojson">
 
-                <MarkerClusterGroup iconCreateFunction={this.createClusterCustomIcon}>
-
-                <Marker position={[59.3023545, -3.2765753]} number={5}>
-                  <Popup
-                    minWidth={200}
-                    closeButton={false}
-                    onClose={popup => console.warn('popup-close', popup)}
-                    >
-                    <div>
-                      <b>Location: Hello world2!</b>
-                      <p>Count: {4}</p>
-                    </div>
-                  </Popup>
-                  </Marker>
-
-                <Marker position={[51.7323545, -3.2765753]} number={5} />
-
-                  <Marker position={[55.7323545, -1.2765753]} number={8} />
-                  <Marker position={[51.7323545, -1.2755753]} number={1000.23} />
-
-              </MarkerClusterGroup>
-
-                <CircleLeaflet center={[51.7323545, -3.2765753]} radius={5000} attribution="hello" >
-                  <Popup
-                    minWidth={200}
-                    closeButton={false}
-                    onClose={popup => console.warn('popup-close', popup)}
-                    >
-                    <div>
-                      <b>Hello world2!</b>
-                      <p>stuff</p>
-                    </div>
-                  </Popup>
-
-                </CircleLeaflet>
-
-                {/* // <GeoJSON data={this.getGeoJSONNew()} style={this.getStyle(1, 2)} /> */}
-                <GeoJSON data={this.getGeoJSONNew()} style={this.style()} />
-              </Maps>
+                  </GeoJSON>
 
 
+
+                </Maps>
+
+
+
+              </Grid>
+              <Grid item xs={12} sm={6} md={6}>
+                <SimpleTabs handleAreaName={this.handleAreaName} areaName={this.state.areaName} areaType={this.state.areaType} dataGottenBackFromAPI={this.state.dataGottenBackFromAPI} />
+              </Grid>
 
             </Grid>
-            <Grid item xs={12} sm={6} md={6}>
-              <SimpleTabs handleAreaName={this.handleAreaName} areaName={this.state.areaName} areaType={this.state.areaType} dataGottenBackFromAPI={this.state.dataGottenBackFromAPI} />
-            </Grid>
-
           </Grid>
-        </Grid>
-      );
+        );
+      }
     }
-  }
 
-  export default App;
+    export default App;
